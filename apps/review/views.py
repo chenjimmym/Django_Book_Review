38c8a6289054959181dd1e5bed37d47fad1a2b05
Jimmy <chen.jimmym@gmail.com>
2017-10-23 09:56:18 -0700

    hashed password

--- apps/review/views.py
+++ apps/review/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from models import *
 from django.contrib import messages
+import bcrypt
 # Create your views here.
 def index(request):
     # Book.objects.all().delete
@@ -14,19 +15,28 @@ def add_user(request):
                 messages.error(request, error)
             return redirect('/')
         else:
-            User.objects.create(name=request.POST['name'], alias=request.POST['alias'], email=request.POST['email'], password=request.POST['password'])
+            password = request.POST['password']
+            hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            User.objects.create(name=request.POST['name'], alias=request.POST['alias'], email=request.POST['email'], password=hashed_password)
+            messages.error(request, "Successfully Registered")
             return redirect('/')
 
 def login(request):
     if request.method == 'POST':
         if not 'login_status' in request.session:
             request.session['login_status'] = False
+        # login_data = User.objects.filter(email=request.POST['email']).filter(password=request.POST['password'])
         login_data = User.objects.filter(email=request.POST['email'])
-        if login_data:
+        inputted_password = request.POST['password']
+        print inputted_password
+        stored_password = User.objects.filter(email=request.POST['email']).first().password
+        print stored_password
+        if login_data and bcrypt.checkpw(inputted_password.encode(), stored_password.encode()):
             request.session['login_status'] = {'id':login_data.first().id, 'name':login_data.first().name, 'alias':login_data.first().alias, 'email':login_data.first().email}
             print "Login Status", request.session['login_status']
             return redirect('/books')
         else:
+            messages.error(request, "Email and password does not match")
             return redirect('/')
 
 def logout(request):
@@ -34,31 +44,12 @@ def logout(request):
     return redirect('/')
 
 def books_home(request):
-    # recent_reviews = Book_Review.objects.order_by("-updated_at")[:3].belong_to
     recent_reviews = Book_Review.objects.order_by("-updated_at")[:3]
-    # title0 = Book.objects.get(id=recent_reviews[0].belong_to_id).title
-    # title1 = Book.objects.get(id=recent_reviews[1].belong_to_id).title
-    # title2 = Book.objects.get(id=recent_reviews[2].belong_to_id).title
-    # reviewed_by0 = User.objects.get(id=recent_reviews[0].reviewed_by_id).name
-    # reviewed_by1 = User.objects.get(id=recent_reviews[1].reviewed_by_id).name
-    # reviewed_by2 = User.objects.get(id=recent_reviews[2].reviewed_by_id).name
-    # message0 =  recent_reviews[0].message
-    # message1 =  recent_reviews[1].message
-    # message2 =  recent_reviews[2].message
-    # rating0 =  recent_reviews[0].rating
-    # rating1 =  recent_reviews[1].rating
-    # rating2 =  recent_reviews[2].rating
-    # block0 = {'title':title0, 'reviewed_by':reviewed_by0, 'message':message0, 'rating':rating0}
-    # block1 = {'title':title1, 'reviewed_by':reviewed_by1, 'message':message1, 'rating':rating1}
-    # block2 = {'title':title2, 'reviewed_by':reviewed_by2, 'message':message2, 'rating':rating2}
     all_books = Book.objects.all()
-    # print '@@@@@@@', recent_reviews
-    # reviews_and_books = {'one':block0, 'two':block1, 'three':block2, 'all_books':all_books}
     context = {
         'recent_reviews': recent_reviews,
         'all_books': all_books
     }
-    
     return render(request, 'review/books.html', context)
 
 def add(request):
