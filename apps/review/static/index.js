ff8779acf55325192b32dcfd5a5f387eff11d870
Jimmy <chen.jimmym@gmail.com>
2017-11-24 19:24:18 -0800

    login page done

--- apps/review/static/index.js
+++ apps/review/static/index.js
@@ -1,16 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#register").click(function(){
-        // alert("clicked");
-        $(".login").fadeOut(function(){
-            $(".registration").fadeIn();
+    function start() {
+        $(".login").slideDown(function(){
+            $("#footer").slideDown();
         });
-    })
 
-    $("#login").click(function(){
-        // alert("clicked");
-        $(".registration").fadeOut(function(){
-            $(".login").fadeIn();
-        });
-    })
+        $("#register").click(function () {
+            // alert("clicked");
+            $(".login").fadeOut(function () {
+                $(".registration").fadeIn();
+            });
+        })
+
+        $("#login").click(function () {
+            // alert("clicked");
+            $(".registration").fadeOut(function () {
+                $(".login").fadeIn();
+            });
+        })
+    }
+
+
+    var word = "elcome to the Book Review!";
+    var el = document.getElementById('title_scroll');
+    var character_counter = 0;
+    // var finished = false;
+    function updateText() {
+        el.innerHTML = el.innerHTML + word[character_counter++];
+        if (character_counter == word.length + 1) {
+            character_counter = word.length;
+            el.innerHTML = 'Welcome to the Book Review!';
+            start();
+            clearInterval(interval);
+            // finished = true;
+        }
+    }
+
+    var interval = setInterval(updateText, 28);
+
 })
